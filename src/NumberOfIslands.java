be57f405ab0e53ecc187d3a87b15ffcf342997f9
alivia-23 <guin.al@husky.neu.edu>
2024-10-19 03:19:04 -0700

    number of islands

---
+++ src/NumberOfIslands.java
@@ -0,0 +1,70 @@
+/**
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * Example 2:
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ *
+ */
+    /**
+     * Questions to ask:- Constraints
+     * What is the range of the grid?
+     * number of rows >= 1
+     * number of columns <= 300
+     * grid contains what type of element -> character value of 0 or 1
+     */
+public class NumberOfIslands {
+    private static int[][] directions = {
+            {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+    };
+    public static int findNumberOfIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        for (int[] dir : directions) {
+            int r = i + dir[0];
+            int c = j + dir[1];
+            dfs(grid, r, c);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+        System.out.println(findNumberOfIslands(grid));
+    }
+
+}
