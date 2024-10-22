4325c41d1484dee34e2172aefabe72a2cedfe212
alivia-23 <guin.al@husky.neu.edu>
2024-10-22 02:12:40 -0700

    two pointer approach to remove nth node

--- src/RemoveNthNodeFromLinkedList.java
+++ src/RemoveNthNodeFromLinkedList.java
@@ -29,6 +29,11 @@ public class RemoveNthNodeFromLinkedList {
         }
     }
 
+    /**
+     * Time complexity - O(L) - The algorithm makes two traversal of the list, first to calculate list length L and second to find the (L−n) th node.
+     *                          There are 2L−n operations and time complexity is O(L)
+     * Space complexity - O(1)
+     */
     public ListNode removeNthNode(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
@@ -45,6 +50,34 @@ public class RemoveNthNodeFromLinkedList {
             current = current.next;
         }
         current = current.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * Two pointer approach:-
+     * Time complexity - O(L) - traverses one pass of L nodes
+     * Space complexity - O(1)
+     */
+    public ListNode removeNthNodeTwoPointer(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        //move fast pointer to the (n + 1)th node where we need to delete
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        // now move both pointers together untill fast reaches null
+        // when fast reaches null at that time slow will reach to the
+        // node before the node to be deleted
+        // then simply connect next pointer of slow to 2 step ahead
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+
         return dummy.next;
     }
 
