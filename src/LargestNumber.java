bc568d9f31baf53324d1b00e7164da817fdd5c91
alivia-23 <guin.al@husky.neu.edu>
2024-10-30 20:22:45 -0700

    edge case handle

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -24,6 +24,8 @@ public class LargestNumber {
 
         Arrays.sort(s, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
 
+        if (s[0].charAt(0) == '0') return "0";
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length; i++) {
             sb.append(s[i]);
@@ -34,8 +36,10 @@ public class LargestNumber {
     public static void main(String[] args) {
         int[] nums = {10, 2};
         int[] nums1 = {3,30,34,5,9};
+        int[] nums2 = {0,0};
         System.out.println(largestNumber(nums));
         System.out.println(largestNumber(nums1));
+        System.out.println(largestNumber(nums2));
     }
 
 }
